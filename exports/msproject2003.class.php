2ed8e76539b37d2dc2ee77dd717e8cdf052ce538
eureka2 <eureka@dbmail.com>
2012-06-04 19:24:11 +0200

    Initial commit

---
+++ exports/msproject2003.class.php
@@ -0,0 +1,566 @@
+<?php /* $Id$ $URL$ */
+if (!defined('W2P_BASE_DIR')){
+	die('You should not access this file directly.');
+}
+
+require_once "msobject.class.php";
+
+class MSProject2003 extends MSObject
+{
+
+  protected $project = array();
+  protected $tasknum = 0;
+
+  public function __construct($exporter, &$project)
+  {
+    parent::__construct($exporter);
+    $this->project = $project;
+  }
+
+  public function dump()
+  {
+    return array(
+
+      // The unique ID of the project.
+      'UID' => $this->project['project_short_name'],
+
+      // The name of the project.
+      'Name' => html_entity_decode(
+        $this->project['project_file'], 
+        ENT_COMPAT, 
+        "UTF-8"
+      ),
+
+      // The title of the project.
+      'Title' => html_entity_decode(
+        $this->project['project_name'], 
+        ENT_COMPAT,
+        "UTF-8"
+      ),
+
+      // The category the project belongs to.
+      'Category' => html_entity_decode(
+        $this->project['project_category'], 
+        ENT_COMPAT, 
+        "UTF-8"
+      ),
+
+      // The company that owns the project.
+      'Company' => html_entity_decode(
+        $this->project['company_name'], 
+        ENT_COMPAT, 
+        "UTF-8"
+      ),
+
+      // The manager of the project.
+      'Manager' => html_entity_decode(
+        $this->project['owner'], 
+        ENT_COMPAT, 
+        "UTF-8"
+      ),
+
+      // The author of the project.
+      'Author' => html_entity_decode(
+        $this->project['creator'], 
+        ENT_COMPAT, 
+        "UTF-8"
+      ),
+
+      // The date that the project was created.
+      'CreationDate' => $this->dumpDateTime(
+        array(
+          $this->project['project_created'], 
+          $this->project['project_start_date']
+        )
+      ),
+
+      // The number of times the project has been saved.
+      // 'Revision' => 1,
+
+      // The date that the project was last saved.
+      'LastSaved' => $this->dumpDateTime(
+        array(
+          $this->project['project_updated'], 
+          $this->project['project_created'], 
+          $this->project['project_start_date']
+        )
+      ),
+
+      // Indicates whether the project is scheduled from 
+      // its start date or finish date.
+      'ScheduleFromStart' => 1,
+
+      // The date and time that a project is scheduled to begin; 
+      // required if ScheduleFromStart is True.
+      'StartDate' => $this->dumpDateTime($this->project['project_start_date']),
+
+      // The date and time that a project is scheduled to end; 
+      // required if ScheduleFromStart is False.
+      'FinishDate' => $this->dumpDateTime($this->project['project_end_date']),
+
+      // The month the fiscal year begins.
+      'FYStartDate' => w2PgetParam($this->exporter->options, 'FYStartDate', '1'),
+
+      // The number of days past its end date that a task can go before 
+      // Microsoft Office Project 2003 marks that task as a critical task.
+      'CriticalSlackLimit' => 0,
+
+      // The number of digits that appear after the decimal 
+      // when currency values are shown in Project.
+      'CurrencyDigits' => $this->project['currency_digits'],
+
+      // The currency symbol used to represent the type of currency 
+      // used in the project.
+      'CurrencySymbol' => html_entity_decode(
+        w2PgetConfig('currency_symbol'), 
+        ENT_COMPAT, 
+        "UTF-8"
+      ),
+
+      // Indicates the placement of the currency symbol 
+      // in relation to the currency value: 
+      'CurrencySymbolPosition' => $this->project['currency_symbol_position'],
+
+      // The unique ID for the calendar used in the project.
+      'CalendarUID' => 1,
+
+      // The default start time for all new tasks.
+      'DefaultStartTime' => w2PgetConfig('cal_day_start') . ':00:00',
+
+      // The default finish time for all new tasks.
+      'DefaultFinishTime' => w2PgetConfig('cal_day_end') . ':00:00',
+
+      // The default number of minutes per day.
+      'MinutesPerDay' => (
+        $mins_per_day = (
+          w2PgetConfig('cal_day_end') - w2PgetConfig('cal_day_start')
+        ) * 60
+      ),
+
+      // The default number of minutes per week.
+      'MinutesPerWeek' => ($mins_per_day * 
+          count(explode(',', w2PgetConfig('cal_working_days')))
+      ),
+
+      // The default number of working days per month.
+      'DaysPerMonth' => (4 * count(explode(',', w2PgetConfig('cal_working_days')))),
+
+      // The default type for all new tasks in the project.
+      // Values are: 0=Fixed Units, 1=Fixed Duration, 2=Fixed Work.
+      'DefaultTaskType' => w2PgetParam(
+        $this->exporter->options, 
+        'DefaultTaskType', 
+        '0'
+      ),
+
+      // The default measuring point when fixed costs are accrued.
+      // Values are: 1=Start, 2=Prorated, 3=End
+      'DefaultFixedCostAccrual' => w2PgetParam(
+        $this->exporter->options, 
+        'DefaultFixedCostAccrual', 
+        '3'
+      ),
+
+      // The default standard rate for new resources.
+      'DefaultStandardRate' => w2PgetParam(
+        $this->exporter->options, 
+        'DefaultStandardRate', 
+        '0'
+      ),
+
+      // The default overtime rate for new resources.
+      'DefaultOvertimeRate' => w2PgetParam(
+        $this->exporter->options, 
+        'DefaultOvertimeRate', 
+        '0'
+      ),
+
+      // The default format for all durations in the project
+      // Values are: 
+      // 3=m, 4=em, 5=h, 6=eh, 7=d, 8=ed, 9=w, 10=ew, 11=mo, 12=emo, 19=%, 20=e%, 
+      // 21=null, 35=m?, 36=em?, 37=h?, 38=eh?, 39=d?, 40=ed?, 41=w?, 42=ew?, 
+      // 43=mo?, 44=emo?, 51=%?, 52=e%? and 53=null.
+      'DurationFormat' => w2PgetParam(
+        $this->exporter->options, 
+        'DurationFormat', 
+        '5'
+      ),
+
+      // The default format for all work durations in the project.
+      // Values are: 1=m, 2=h, 3=d, 4=w, 5=mo
+      'WorkFormat' => w2PgetParam(
+        $this->exporter->options, 
+        'WorkFormat', 
+        '2'
+      ),
+
+      // Indicates whether Project automatically calculates actual costs.
+      'EditableActualCosts' => 0,
+
+      // Indicates whether Project schedules tasks according to their 
+      // constraint dates instead of any task dependencies.
+      'HonorConstraints' => 0,
+
+      // The default method for calculating earned value.
+      // Values are: 0=Percent Complete, 1=Physical Percent Complete
+      'EarnedValueMethod' => 0,
+
+      // Indicates whether inserted projects are treated as summary tasks 
+      // rather than as separate projects for schedule calculation.
+      'InsertedProjectsLikeSummary' => 1,
+
+      // Indicates whether Project calculates and displays a critical path for 
+      // each independent network of tasks within a project.
+      'MultipleCriticalPaths' => 0,
+
+      // Indicates whether new tasks are effort-driven.
+      'NewTasksEffortDriven' => w2PgetParam(
+        $this->exporter->options, 
+        'NewTasksEffortDriven', 
+        '0'
+      ),
+
+      // Indicates whether new tasks have estimated durations.
+      'NewTasksEstimated' =>  w2PgetParam(
+        $this->exporter->options, 
+        'NewTasksEstimated', 
+        '0'
+      ),
+
+      // Indicates whether in-progress tasks may be split.
+      'SplitsInProgressTasks' => 1,
+
+      // Indicates whether actual costs are spread to the status date.
+      'SpreadActualCost' => 0,
+
+      // Indicates whether percent complete is spread to the status date.
+      'SpreadPercentComplete' => 0,
+
+      // Indicates whether updates to tasks update resources.
+      'TaskUpdatesResource' => 1,
+
+      // Indicates whether fiscal year numbering is used.
+      'FiscalYearStart' => 0,
+
+      // The start day of the week.
+      'WeekStartDay' => LOCALE_FIRST_DAY,
+
+      // Indicates whether the end of completed portions of tasks scheduled 
+      // to begin after the status date, but begun early, 
+      // should be moved back to the status date.
+      'MoveCompletedEndsBack' => 0,
+
+      // Indicates whether the beginning of remaining portions of tasks scheduled 
+      // to begin after the status date, but begun early, 
+      // should be moved back to the status date.
+      'MoveRemainingStartsBack' => 0,
+
+      // Indicates whether the beginning of remaining portions of tasks scheduled 
+      // to have begun late should be moved up to the status date.
+      'MoveRemainingStartsForward' => 0,
+
+      // Indicates whether the end of completed portions of tasks scheduled 
+      // to have been completed before the status date, but begun late, 
+      // should be moved up to the status date.
+      'MoveCompletedEndsForward' => 0,
+
+      // The specific baseline used to calculate Variance values.
+      // Values are: 0=Baseline, 1=Baseline 1, 2=Baseline 2, 3=Baseline 3, 
+      // 4=Baseline 4, 5=Baseline 5, 6=Baseline 6, 7=Baseline 7, 8=Baseline 8, 
+      // 9=Baseline 9, 10=Baseline 10
+      'BaselineForEarnedValue' => 0,
+
+      // Indicates whether to automatically add new resources to the resource pool.
+      'AutoAddNewResourcesAndTasks' => w2PgetParam(
+        $this->exporter->options, 
+        'AutoAddNewResourcesAndTasks', 
+        '0'
+      ),
+
+      // Date used for calculation and reporting.
+      'StatusDate' => '',
+
+      // The system date that the XML was generated.
+      'CurrentDate' => date('Y-m-d\TH:i:s'),
+
+      // Indicates whether the project was created by a 
+      // Microsoft Office Project Server 2003 user or a Microsoft Windows NT user.
+      'MicrosoftProjectServerURL' => 1,
+
+      // Indicates whether to autolink inserted or moved tasks.
+      'Autolink' => w2PgetParam(
+        $this->exporter->options, 
+        'Autolink', 
+        '0'
+      ),
+
+      // The default start date for a new task. 
+      // Values are: 0=Project Start Date, 1=Current Date
+      'NewTaskStartDate' => w2PgetParam(
+        $this->exporter->options, 
+        'NewTaskStartDate', 
+        '0'
+      ),
+
+      // The default earned value method for tasks. 
+      // Values are: 0=Percent Complete, 1=Physical Percent Complete
+      'DefaultTaskEVMethod' => w2PgetParam(
+        $this->exporter->options, 
+        'DefaultTaskEVMethod', 
+        '0'
+      ),
+
+      // Indicates whether the project was edited externally.
+      'ProjectExternallyEdited' => 1,
+
+      // Date used for calculation and reporting.
+      'ExtendedCreationDate' => '',
+
+      // Indicates whether all actual work has been synchronized with the project.
+      'ActualsInSync' => 0,
+
+      // Indicates whether to remove all file properties on save.
+      'RemoveFileProperties' => 0,
+
+      // Indicates whether the project is an administrative project.
+      'AdminProject' => 0,
+
+      // The collection of outline code definitions associated with the project; 
+      // these codes may be associated with any number of projects.
+      'OutlineCodes' => '',
+
+      // The table of entries that define an outline code mask.
+      'WBSMasks' => '',
+
+      // The collection of extended attribute (custom field) definitions 
+      // associated with a project.
+      'ExtendedAttributes' => $this->dumpExtendedAttributes(),
+
+      // The collection of calendars associated with the project.
+      'Calendars' => $this->dumpCalendars(),
+
+      // The collection of tasks that make up the project.
+      'Tasks' => $this->exportTasks(),
+
+      // The collection of resources that make up the project.
+      'Resources' => $this->dumpResources(),
+
+      // The collection of assignments that make up the project.
+      'Assignments' => $this->dumpAssignments(),
+
+    );
+  }
+
+  protected function dumpCalendars() 
+  {
+    $calendars = array();
+    $working_days = explode(",", w2PgetConfig("cal_working_days"));
+    $weekdays = array();
+    for ($i = 1; $i <= 7; $i++) {
+      $dayworking = 0;
+      $workingtimes = array();
+      if (in_array($i - 1, $working_days)) {
+        $dayworking = 1;
+        $workingtimes[] = array(
+
+          // Defines the time worked on the working day. 
+          // One of these must be present, and there may be no more than five..
+          'WorkingTime' => array(
+
+             // The start of the working time.
+            'FromTime' =>w2PgetConfig('cal_day_start') . ':00:00',
+
+             // The end of the working time.
+            'ToTime' =>w2PgetConfig('cal_day_end') . ':00:00'
+          )
+        );
+      }
+      $weekdays[] = array(
+
+          // A weekday defines either regular days of the week 
+          // or exception days in the calendar.
+        'WeekDay' => array(
+
+          // The type of working day (exception, or Monday - Sunday)
+          'DayType' => $i,
+
+          // Indicates whether the specified date or date type is a working day.
+          'DayWorking' => $dayworking,
+
+          // The collection of working times that define the time worked.
+          'WorkingTimes' => $workingtimes
+
+        )
+      );
+    } 
+    foreach ($this->exporter->calendar as $holiday) {
+      $weekdays[] = array(
+
+        // A weekday defines either regular days of the week 
+        // or exception days in the calendar.
+        'WeekDay' => array(
+
+          // The type of working day (exception, or Monday - Sunday)
+          'DayType' => 0,
+
+          // Indicates whether the specified date or date type is a working day.
+          'DayWorking' => 0,
+
+          // Defines a set of exception days.
+          'TimePeriod' => array(
+
+            // The start of the exception time.
+            'FromDate' => $this->dumpDateTime($holiday['startDate']->getDate(), false),
+
+            // The end of the exception time.
+            'ToDate' => $this->dumpDateTime($holiday['endDate']->getDate(), false)
+
+          )
+        )
+      );
+    }
+    $calendars[] = array(
+      'Calendar' => array(
+        'UID' => 1,
+        'Name' => 'Standard',
+        'IsBaseCalendar' => 1,
+        'BaseCalendarUID' => -1,
+        'WeekDays' => $weekdays
+      )
+    );
+    foreach ($this->exporter->resources as $resource){
+      if ($resource['resource_type'] == 1) {
+        $uid = $resource['user_id'];
+        $holidays = $resource['resource_holidays'];
+        $weekdays = array();
+        foreach ($holidays as $holiday) {
+          if ($holiday['type'] != HOLIDAY_TYPE_CALENDAR_HOLIDAY) {
+            $weekdays[] = array(
+              'WeekDay' => array(
+                'DayType' => 0,
+                // Indicates whether the specified date or date type is a working day.
+                'DayWorking' => 0,
+                'TimePeriod' => array(
+                  'FromDate' => $this->dumpDateTime($holiday['startDate']->getDate(), false),
+                  'ToDate' => $this->dumpDateTime($holiday['endDate']->getDate(), false)
+                )
+              )
+            );
+          }
+        }
+        if (!empty($weekdays)) {
+          $calendars[] = array(
+            'Calendar' => array(
+              'UID' => $resource['user_id'],
+              'Name' => html_entity_decode(
+                $resource['resource_name'], 
+                ENT_COMPAT,
+                "UTF-8"
+              ),
+              'IsBaseCalendar' => 0,
+              'BaseCalendarUID' => 1,
+              'WeekDays' => $weekdays
+            )
+          );
+        }
+      }
+    }
+    return $calendars;
+  }
+
+  protected function exportTasks()
+  {
+    $tasks = array();
+    $this->dumpTasksTree($this->exporter->taskstree, $tasks);
+    array_unshift($tasks, array('Task' => $this->dumpProjectSummaryTask()));
+    return $tasks;
+  }
+
+  protected function dumpTasksTree(&$taskstree, &$tasks)
+  {
+    foreach($taskstree as $tasknode) {
+      $task = $tasknode['task'];
+      if (is_null($task['task_created']) || 
+          $task['task_created'] == "0000-00-00 00:00:00") {
+        $task['task_created'] = $this->project['project_created'];
+      }
+      if (is_null($task['task_start_date']) || 
+        $task['task_start_date'] == "0000-00-00 00:00:00") {
+        $task['task_start_date'] = $this->project['project_start_date'];
+      }
+      $task['id'] = ++$this->tasknum;
+      $mstask = $this->factory('Task', $task);
+      $tasks[]= array('Task' => $mstask->dump());
+      if (isset($tasknode['children'])) {
+        $chidren = $tasknode['children'];
+        $this->dumpTasksTree($chidren, $tasks);
+      }
+    }
+  }
+
+  protected function dumpProjectSummaryTask()
+  {
+    $task = array (
+      'task_id' => 0,
+      'task_parent' => 0,
+      'id' => 0,
+      'task_name' => $this->project['project_name'],
+      'task_created' => $this->project['project_created'],
+      'task_updated' => $this->project['project_updated'],
+      'task_start_date' => $this->project['project_start_date'],
+      'task_end_date' => $this->project['project_end_date'],
+      'task_actual_end_date' => $this->project['project_actual_end_date'],
+      'task_owner_contact' => $this->project['owner'],
+      'wbs' => 0,
+      'wbs_level' => 0,
+      'outline_number' => 0,
+      'outline_level' => 0,
+      'task_priority' =>  $this->project['project_priority'],
+      'task_duration' => $this->project['project_duration'],
+      'task_duration_type' => 1,
+      'task_work' => $this->project['project_work'],
+      'task_milestone' => 0,
+      'task_summary' => 1,
+      'task_percent_complete' => $this->project['project_percent_complete'],
+      'task_target_budget' => $this->project['project_target_budget'],
+      'task_actual_duration' => $this->project['project_duration'],
+      'task_hours_worked' => $this->project['project_worked_hours'],
+      'task_regular_work' => 0,
+      'task_remaining_duration' => 0,
+      'task_related_url' => $this->project['project_url'],
+      'task_description' => $this->project['project_description'],
+      'task_predecessors' => array(),
+   );
+   $mstask = $this->factory('Task', $task);
+   return $mstask->dump();
+  }
+
+  protected function dumpResources()
+  {
+    $resources = array();
+    if (count($this->exporter->resources) > 0) {
+      $id = 0;
+      foreach ($this->exporter->resources as $resource){
+        $resource['id'] = ++$id;
+        $msresource = $this->factory('Resource', $resource);
+        $resources[] = array('Resource' => $msresource->dump());
+      }
+    }
+    return $resources;
+  }
+
+  protected function dumpAssignments()
+  {
+    $assignments = array();
+    if (count($this->exporter->assignments) > 0) {
+      $uid = 0;
+      foreach ($this->exporter->assignments as $assignment){
+        $assignment['uid'] = ++$uid;
+        $msassignment = $this->factory('Assignment', $assignment);
+        $assignments[] = array( "Assignment" => $msassignment->dump());
+      }
+    }
+    return $assignments;
+  }
+
+}
